190ec8da5c8bf78a0306c8631881b034eabe4271
Sivakrishnan S <sivakrishnan@colanonline.com>
2025-02-06 17:02:01 +0530

    [Skrish] commit-3

---
+++ WorkOutClassHierarchy/OuterClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpInterview.WorkOutClassHierarchy
+{
+ 
+    public class OuterClass
+    {
+        public void outclassfunction()
+        {
+            Console.WriteLine("outclassfunction called");
+        }
+
+        public class InnerClass
+        {
+            public void innerclassfunction()
+            {
+                Console.WriteLine("innerclassfunction called");
+            }
+        }
+    }
+
+}

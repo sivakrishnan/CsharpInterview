49ba3948fc584b3a26ff346363c4cd8fa18ec8df
siva krishnan <sivakrishnancse052@gmail.com>
2026-01-22 04:50:33 +0530

    [Skrish] Trigger - insert, delete and update

---
+++ SQL/043 Trigger-Insert and delete.sql
@@ -0,0 +1,102 @@
+--video 43: Duration: 17:44 Triggers
+
+--types of triggers
+
+--1) DML Triggers
+--2) DDL Triggers
+--3) Logon Triggers
+
+-- DML Triggers: AFTER Triggers, INSTEAD OF Triggers
+
+
+CREATE TABLE tbl043Employee
+(
+  Id int Primary Key,
+  Name nvarchar(30),
+  Salary int,
+  Gender nvarchar(10),
+  DepartmentId int
+)
+
+Insert into tbl043Employee values (1,'John', 5000, 'Male', 3)
+Insert into tbl043Employee values (2,'Mike', 3400, 'Male', 2)
+Insert into tbl043Employee values (3,'Pam', 6000, 'Female', 1)
+Insert into tbl043Employee values (4,'Pam-trigger', 6000, 'Female', 1)
+
+CREATE TABLE tbl043EmployeeAudit
+(
+  Id int identity(1,1) primary key,
+  AuditData nvarchar(1000)
+)
+
+
+alter TRIGGER tr_tbl043Employee_ForInsert 
+ON tbl043Employee
+FOR INSERT
+AS
+BEGIN
+ Declare @Id int
+ Select @Id = Id from inserted
+
+ select * from inserted
+ 
+ insert into tbl043EmployeeAudit 
+ values('New employee with Id  = ' + Cast(@Id as nvarchar(5)) + ' is added at ' + cast(Getdate() as nvarchar(20)))
+END
+
+select * from tbl043Employee
+select * from tbl043EmployeeAudit
+
+-- IMPORTANT NOTES
+-- our trigger support for only one row inserted, for the below insert statement will capture the first row in audit table 
+
+insert into tbl043Employee (Id, Name, Salary, Gender, DepartmentId)
+select 10 as Id,'Pam-10' as Name, 6000 as Salary, 'Female' as Gender, 1 as DepartmentId
+union
+select 11 as Id,'Pam-11' as Name, 6000 as Salary, 'Female' as Gender, 1 as DepartmentId
+union
+select 12 as Id,'Pam-12' as Name, 6000 as Salary, 'Female' as Gender, 1 as DepartmentId
+union
+select 13 as Id,'Pam-13' as Name, 6000 as Salary, 'Female' as Gender, 1 as DepartmentId
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+
+alter TRIGGER tr_tblEMployee_ForDelete
+ON tbl043Employee
+FOR DELETE
+AS
+BEGIN
+ Declare @Id int
+ Select @Id = Id from deleted
+
+ select * from deleted
+ 
+ if(@Id is null)
+ begin
+		insert into tbl043EmployeeAudit 
+		values('Data Not Available')
+ end
+ else
+ begin
+		insert into tbl043EmployeeAudit 
+		values('An existing employee with Id  = ' + Cast(@Id as nvarchar(5)) + ' is deleted at ' + Cast(Getdate() as nvarchar(20)))
+ end
+
+END
+
+
+delete tbl043Employee where Id=4
+
+-- IMPORTANT NOTES
+-- our trigger support for only one row deleted, for the below delete statement will capture the first row in audit table 
+
+delete from tbl043Employee where Id>=10
+
+select * from tbl043Employee
+select * from tbl043EmployeeAudit
+
+-- when try to delete a row which is not available in the table, then null will be inserted 
+--1	New employee with Id  = 4 is added at Jan 22 2026  4:05AM
+--2	An existing employee with Id  = 4 is deleted at Jan 22 2026  4:09AM
+--3	NULL
---
+++ SQL/044 Trigger-Update.sql
@@ -0,0 +1,88 @@
+--video 44: Duration: 17:18 After update trigger 
+
+select * from tbl043Employee
+select * from tbl043EmployeeAudit
+
+Create trigger tr_tblEmployee_ForUpdate
+on tbl043Employee
+for Update
+as
+Begin
+ Select * from deleted
+ Select * from inserted 
+End
+
+Insert into tbl043Employee values (1,'John', 5000, 'Male', 3)
+Insert into tbl043Employee values (2,'Mike', 3400, 'Male', 2)
+Insert into tbl043Employee values (3,'Pam', 6000, 'Female', 1)
+
+
+delete tbl043Employee where Id=4
+Insert into tbl043Employee values (4,'Pam-trigger', 6000, 'Female', 1)
+
+Update tbl043Employee set Name = 'Tods', Salary = 2000, 
+Gender = 'Female' where Id = 4
+
+
+Alter trigger tr_tblEmployee_ForUpdate
+on tbl043Employee
+for Update
+as
+Begin
+      -- Declare variables to hold old and updated data
+      Declare @Id int
+      Declare @OldName nvarchar(20), @NewName nvarchar(20)
+      Declare @OldSalary int, @NewSalary int
+      Declare @OldGender nvarchar(20), @NewGender nvarchar(20)
+      Declare @OldDeptId int, @NewDeptId int
+     
+      -- Variable to build the audit string
+      Declare @AuditString nvarchar(1000)
+      
+      -- Load the updated records into temporary table
+      Select * into #TempTable from inserted
+     
+      -- Loop thru the records in temp table
+      While(Exists(Select Id from #TempTable))
+      Begin
+            --Initialize the audit string to empty string
+            Set @AuditString = ''
+           
+            -- Select first row data from temp table
+            Select Top 1 @Id = Id, @NewName = Name, 
+            @NewGender = Gender, @NewSalary = Salary,
+            @NewDeptId = DepartmentId
+            from #TempTable
+           
+            -- Select the corresponding row from deleted table
+            Select @OldName = Name, @OldGender = Gender, 
+            @OldSalary = Salary, @OldDeptId = DepartmentId
+            from deleted where Id = @Id
+   
+     -- Build the audit string dynamically           
+            Set @AuditString = 'Employee with Id = ' + Cast(@Id as nvarchar(4)) + ' changed'
+            if(@OldName <> @NewName)
+                  Set @AuditString = @AuditString + ' NAME from ' + @OldName + ' to ' + @NewName
+                 
+            if(@OldGender <> @NewGender)
+                  Set @AuditString = @AuditString + ' GENDER from ' + @OldGender + ' to ' + @NewGender
+                 
+            if(@OldSalary <> @NewSalary)
+                  Set @AuditString = @AuditString + ' SALARY from ' + Cast(@OldSalary as nvarchar(10))+ ' to ' + Cast(@NewSalary as nvarchar(10))
+                  
+     if(@OldDeptId <> @NewDeptId)
+                  Set @AuditString = @AuditString + ' DepartmentId from ' + Cast(@OldDeptId as nvarchar(10))+ ' to ' + Cast(@NewDeptId as nvarchar(10))
+           
+            insert into tbl043EmployeeAudit values(@AuditString)
+            
+            -- Delete the row from temp table, so we can move to the next row
+            Delete from #TempTable where Id = @Id
+      End
+End
+
+
+
+update tbl043Employee set Gender='Male' where Id>=10
+
+select * from tbl043Employee where Id>=10
+select * from tbl043EmployeeAudit

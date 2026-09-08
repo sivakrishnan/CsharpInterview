4ac4259f9bebe50d435a9b86d7672afa5228f046
siva krishnan <sivakrishnancse052@gmail.com>
2026-09-08 06:58:37 +0530

    [Skrish] Python started

---
+++ 011 python/script.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+first_name="skrish"
+last_name="suyambu"
+template="Hi {} {}"
+print(template.format(first_name,last_name))
+print(f"Hi {first_name} {last_name}")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
